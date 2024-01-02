dc2684aa647bbd14e16a9cc21139d0abf67dc552
Alice Grosch <a.grosch@fz-juelich.de>
2024-01-02 15:33:37 +0100

    Initial 4.0.2 commit

--- static/js/home/handle-servers.js
+++ static/js/home/handle-servers.js
@@ -1,10 +1,11 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi", "utils"], function (
+require(["jquery", "jhapi", "utils", "home/utils"], function (
   $,
   JHAPI,
   utils,
+  custom_utils
 ) {
   "use strict";
 
@@ -13,14 +14,11 @@ require(["jquery", "jhapi", "utils"], function (
   var api = new JHAPI(base_url);
 
   function cancelServer() {
-    var tr = _getTr(this);
-    const id = tr.data("server-id");
+    var [tr, id] = _getTrAndId(this);
     _disableTrButtons(tr);
     api.cancel_named_server(user, id, {
       success: function () {
         console.log("cancel success");
-        // If cancelling, we want to keep to keep the
-        // progress indicator,so we do not reset the progress bar
       },
       error: function () {
         console.log("cancel error");
@@ -29,22 +27,21 @@ require(["jquery", "jhapi", "utils"], function (
   }
 
   function stopServer() {
-    var tr = _getTr(this);
-    const id = tr.data("server-id");
+    var [tr, id] = _getTrAndId(this);
     _disableTrButtons(tr);
-    $(`#${id}-progress-bar`).removeClass("bg-success").addClass("bg-danger");
-    $(`#${id}-progress-info-text`).html("stopping...");
+    custom_utils.updateProgressState(id, "stopping");
     api.stop_named_server(user, id, {
       success: function () {
         console.log("stop success");
-        _enableTrButtonsNonRunning(tr);
-        _resetProgress(id);
+        let running = false;
+        _enableTrButtons(tr, running);
+        // Reset progress
+        custom_utils.updateProgressState(id, "reset");
       },
       error: function (xhr) {
         console.log("stop error");
+        custom_utils.updateProgressState(id, "stop_failed");
         tr.find(".btn-open-lab, .btn-cancel-lab").removeClass("disabled");
-        $(`#${id}-progress-bar`).removeClass("bg-danger").addClass("bg-success");
-        $(`#${id}-progress-info-text`).html("running (stop failed)");
         $(`#${id}-log`)
           .append($('<div class="log-div">')
             .html`Could not stop server. Error: ${xhr.responseText}`);
@@ -53,9 +50,8 @@ require(["jquery", "jhapi", "utils"], function (
   }
 
   function deleteServer() {
-    var collapsibleTr = _getTr(this);
     var that = $(this);
-    const id = collapsibleTr.data("server-id");
+    var [collapsibleTr, id] = _getTrAndId(this);
     _disableTrButtons(collapsibleTr);
     api.delete_named_server(user, id, {
       success: function () {
@@ -72,40 +68,38 @@ require(["jquery", "jhapi", "utils"], function (
   }
 
   function startServer() {
-    var tr = _getTr(this);
-    const id = tr.data("server-id");
-    var collapsibleTr = tr.siblings(`.lab-collapsible-tr[data-server-id=${id}]`);
-
+    var [tr, id] = _getTrAndId(this);
+    var collapsibleTr = tr.siblings(`.collapsible-tr[data-server-id=${id}]`);
     _disableTrButtons(tr);
+
     // Validate the form and start spawn only after validation
     try {
       $(`form[id*=${id}]`).submit();
     }
     catch (e) {
-      _enableTrButtonsNonRunning(tr);
+      let running = false;
+      _enableTrButtons(tr, running);
       return;
     }
-
-    // Update the summary row according to the values set in the collapsibleTr
-    _updateTr(tr, collapsibleTr);
-    // Set the href of the start button to the spawn url
-    _resetProgress(id);
+    custom_utils.updateProgressState(id, "reset");
     $(`#${id}-log`).html("");
 
     var options = _createDataDict(collapsibleTr);
+    // Update the summary row according to the values set in the collapsibleTr
+    _updateTr(tr, id, options);
     // Open a new tab for spawn_pending.html
-    // Need to create it here for context reasons
+    // Need to create it here for JS context reasons
     var newTab = window.open("about:blank");
     api.start_named_server(user, id, {
       data: JSON.stringify(options),
       success: function () {
         // Save latest log to time stamp and empty it
-        _updateSpawnEventsAndLog(id);
-        // Update global user options
-        userOptions[id] = options;
+        custom_utils.updateSpawnEvents(window.spawnEvents, id);
+        window.userOptions[id] = options;
         // Open the spawn url in the new tab
         newTab.location.href = utils.url_path_join(base_url, "spawn", user, id);
         // Hook up event-stream for progress
+        var evtSources = window.evtSources;
         if (!(id in evtSources)) {
           var progressUrl = utils.url_path_join(jhdata.base_url, "api/users", jhdata.user, "servers", id, "progress");
           progressUrl = progressUrl + "?_xsrf=" + window.jhdata.xsrf_token;
@@ -115,7 +109,8 @@ require(["jquery", "jhapi", "utils"], function (
           }
         }
         // Successfully sent request to start the lab, enable row again
-        _enableTrButtonsRunning(tr);
+        let running = true;
+        _enableTrButtons(tr, running);
       },
       error: function (xhr) {
         newTab.close();
@@ -125,42 +120,38 @@ require(["jquery", "jhapi", "utils"], function (
           document.location.reload();
           return;
         }
-        // Update progress in tr
-        $(`#${id}-progress-bar`)
-          .width(100)
-          .removeClass(".bg-success")
-          .addClass(".bg-danger");
-        $(`#${id}-progress-info-text`).html("last spawn failed");
+        custom_utils.updateProgressState(id, "failed");
         // Update progress in log
         let details = $("<details>")
           .append($("<summary>")
             .html(`Could not request spawn. Error: ${xhr.responseText}`))
           .append($("<pre>")
-            .html(_parseJSON(xhr.responseText)));
-        let div = $("<div>").addClass("log-div").html(details);
-        $(`#${id}-log`).append(div);
+            .html(custom_utils.parseJSON(xhr.responseText)));
+        $(`#${id}-log`).append(
+          $("<div>").addClass("log-div").html(details)
+        );
         // Spawn attempt finished, enable row again
-        _enableTrButtonsNonRunning(tr);
+        let running = false;
+        _enableTrButtons(tr, running);
       }
     });
   }
 
   function startNewServer() {
-    function uuidv4hex() {
+    function _uuidv4hex() {
       return ([1e7, 1e3, 4e3, 8e3, 1e11].join('')).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
     }
 
-    function uuidWithLetterStart() {
-      let uuid = uuidv4hex();
+    function _uuidWithLetterStart() {
+      let uuid = _uuidv4hex();
       let char = Math.random().toString(36).match(/[a-zA-Z]/)[0];
       return char + uuid.substring(1);
     }
 
-    const id = "new_jupyterlab";
-    const uuid = uuidWithLetterStart();
+    const uuid = _uuidWithLetterStart();
     // Start button is in collapsible tr for new labs
-    var collapsibleTr = _getTr(this);
+    var [collapsibleTr, id] = _getTrAndId(this);
     _disableTrButtons(collapsibleTr);
 
     // Validate the form and start spawn only after validation
@@ -174,7 +165,7 @@ require(["jquery", "jhapi", "utils"], function (
 
     var options = _createDataDict(collapsibleTr);
     // Open a new tab for spawn_pending.html
-    // Need to create it here for context reasons
+    // Need to create it here for JS context reasons
     var newTab = window.open("about:blank");
     api.start_named_server(user, uuid, {
       data: JSON.stringify(options),
@@ -197,9 +188,10 @@ require(["jquery", "jhapi", "utils"], function (
           .append($("<summary>")
             .html(`Could not request spawn. Error: ${xhr.responseText}`))
           .append($("<pre>")
-            .html(_parseJSON(xhr.responseText)));
-        let div = $("<div>").addClass("log-div").html(details);
-        $(`#${id}-log`).append(div);
+            .html(custom_utils.parseJSON(xhr.responseText)));
+        $(`#${id}-log`).append(
+          $("<div>").addClass("log-div").html(details)
+        );
         collapsibleTr.find("button").removeClass("disabled");
       }
     });
@@ -226,8 +218,8 @@ require(["jquery", "jhapi", "utils"], function (
       var tab = new bootstrap.Tab($("#" + tab_id));
       tab.show();
       // Open the collapsibleTr if it was hidden
-      const id = getId(this);
-      var tr = $(`.lab-summary-tr[data-server-id=${id}`);
+      const id = custom_utils.getId(this);
+      var tr = $(`.summary-tr[data-server-id=${id}`);
       if (!$(`${id}-collapse`).css("display") == "none") {
         tr.trigger("click");
       }
@@ -247,9 +239,8 @@ require(["jquery", "jhapi", "utils"], function (
   Save and revert changes to spawner
   */
   function saveChanges() {
-    var collapsibleTr = _getTr(this);
-    const id = getId(this);
-    var tr = $(`.lab-summary-tr[data-server-id=${id}]`);
+    var [collapsibleTr, id] = _getTrAndId(this);
+    var tr = $(`.summary-tr[data-server-id=${id}]`);
     var alert = $(this).siblings(".alert");
 
     const displayName = _getDisplayName(collapsibleTr);
@@ -257,7 +248,7 @@ require(["jquery", "jhapi", "utils"], function (
     api.update_named_server(user, id, {
       data: JSON.stringify(options),
       success: function () {
-        _updateTr(tr, collapsibleTr);
+        _updateTr(tr, id, options);
         // Update global user options
         userOptions[id] = options;
         alert.children("span")
@@ -277,15 +268,15 @@ require(["jquery", "jhapi", "utils"], function (
     var alert = $(this).siblings(".alert");
 
     const options = userOptions[id];
-    if (options) var name = options["name"];
-    else var name = "new JupyterLab";
+    var name = options["name"];
 
     api.update_named_server(user, id, {
       data: JSON.stringify(options),
       success: function () {
         $(`#${id}-name-input`).val(options["name"]);
         // Reset all user inputs to the values saved in the global user options
-        setUserOptions(id, options);
+        let available = checkIfAvailable(id, options);
+        setUserOptions(id, options, available);
         // Remove all tab warnings since manual changes shouldn't cause warnings
         $("[id$=tab-warning]").addClass("invisible");
         // Show first tab after resetting values
@@ -316,8 +307,10 @@ require(["jquery", "jhapi", "utils"], function (
     return displayName;
   }
 
-  function _getTr(element) {
-    return $(element).parents("tr");
+  function _getTrAndId(element) {
+    let tr = $(element).parents("tr");
+    let id = tr.data("server-id");
+    return [tr, id];
   }
 
   function _disableTrButtons(tr) {
@@ -325,34 +318,27 @@ require(["jquery", "jhapi", "utils"], function (
     tr.find(".btn").addClass("disabled");
   }
 
-  function _enableTrButtonsRunning(tr) {
-    // Show open/cancel for starting labs
-    tr.find(".btn-na-lab, .btn-start-lab").addClass("d-none");
-    tr.find(".btn-open-lab, .btn-cancel-lab").removeClass("d-none");
-    // Disable until fitting event received from EventSource
-    tr.find(".btn-open-lab, .btn-cancel-lab").addClass("disabled");
-  }
-
-  function _enableTrButtonsNonRunning(tr) {
-    var na = tr.find(".na-status").text() || 0;
-    // Show start or na for non-running labs
-    if (na != "0") {
-      tr.find(".btn-na-lab").removeClass("d-none disabled");
-      tr.find(".btn-start-lab").addClass("d-none");
+  function _enableTrButtons(tr, running) {
+    if (running) {
+      // Show open/cancel for starting labs
+      tr.find(".btn-na-lab, .btn-start-lab").addClass("d-none");
+      tr.find(".btn-open-lab, .btn-cancel-lab").removeClass("d-none");
+      // Disable until fitting event received from EventSource
+      tr.find(".btn-open-lab, .btn-cancel-lab").addClass("disabled");
     }
     else {
-      tr.find(".btn-na-lab").addClass("d-none")
-      tr.find(".btn-start-lab").removeClass("d-none disabled");
+      // Show start or na for non-running labs
+      var na = tr.find(".na-status").text() || 0;
+      if (na != "0") {
+        tr.find(".btn-na-lab").removeClass("d-none disabled");
+        tr.find(".btn-start-lab").addClass("d-none");
+      }
+      else {
+        tr.find(".btn-na-lab").addClass("d-none")
+        tr.find(".btn-start-lab").removeClass("d-none disabled");
+      }
+      tr.find(".btn-open-lab, .btn-cancel-lab, .btn-stop-lab").addClass("d-none");
     }
-    tr.find(".btn-open-lab, .btn-cancel-lab, .btn-stop-lab").addClass("d-none");
-  }
-
-  function _resetProgress(id) {
-    let progressBar = $(`#${id}-progress-bar`);
-    let infoText = $(`#${id}-progress-info-text`);
-    progressBar.width(0).html("");
-    progressBar.removeClass("bg-danger bg-success");
-    infoText.html("");
   }
 
   function _showErrorAlert(alert, name, text) {
@@ -363,12 +349,18 @@ require(["jquery", "jhapi", "utils"], function (
       .addClass("alert-danger show p-1");
   }
 
-  function _parseJSON(inputString) {
-    try {
-      return JSON.stringify(JSON.parse(inputString), null, 2);
-    } catch (e) {
-      return inputString;
+
+  function _updateTr(tr, id, options) {
+    tr.find(".name-td").text(options.name);
+    function _updateTd(key) {
+      let configTdDiv = tr.find(`#${id}-config-td-div-${key}`);
+      if (options[key]) configTdDiv.removeClass("d-none");
+      else configTdDiv.addClass("d-none");
+      let configDiv = tr.find(`#${id}-config-td-${key}`);
+      configDiv.text(options[key]);
     }
+    ["system", "flavor", "partition", "project",
+      "runtime", "nodes", "gpus"].forEach(key => _updateTd(key));
   }
 
   function _createDataDict(collapsibleTr) {
@@ -379,7 +371,7 @@ require(["jquery", "jhapi", "utils"], function (
       var select = collapsibleTr.find(`select[id*=${param}]`);
       var value = select.val();
       if (param == "version") {
-        param = "service"
+        param = "profile"
         value = "JupyterLab/" + value;
       }
       if (value) options[param] = value;
@@ -388,10 +380,7 @@ require(["jquery", "jhapi", "utils"], function (
     function _addInputValue(param) {
       var input = collapsibleTr.find(`input[id*=${param}]`);
       var value = input.val();
-      if (param == "runtime") {
-        value = value * 60;
-      }
-      else if (param == "xserver") {
+      if (param == "xserver") {
         if (!collapsibleTr.find(`input[id*=xcbserver-input]`)[0].checked) return;
       }
       if (value) options[param] = value;
@@ -408,39 +397,10 @@ require(["jquery", "jhapi", "utils"], function (
       options[param] = values;
     }
 
-    _addSelectValue("version"); // service
-    _addSelectValue("system");
-    _addSelectValue("account");
-    _addSelectValue("project");
-    _addSelectValue("partition");
-    _addSelectValue("reservation");
-    _addInputValue("nodes");
-    _addInputValue("gpus");
-    _addInputValue("runtime");
-    _addInputValue("xserver");
-    _addCbValues("additional_spawn_options");
+    ["version", "system", "flavor", "account",
+      "project", "partition", "reservation"].forEach(key => _addSelectValue(key));
+    ["nodes", "gpus", "runtime", "xserver"].forEach(key => _addInputValue(key));
+    _addCbValues("userModules");
     return options;
-  }
-
-  function _updateTr(tr, collapsibleTr) {
-    const displayName = _getDisplayName(collapsibleTr);
-    tr.find(".name-td").text(displayName);
-    tr.find(".system-td").text(collapsibleTr.find("select[id*=system]").val());
-    tr.find(".partition-td").text(collapsibleTr.find("select[id*=partition]").val());
-    tr.find(".project-td").text(collapsibleTr.find("select[id*=project]").val());
-  }
-
-  function _updateSpawnEventsAndLog(id) {
-    const startEvent = spawnEvents[id]["latest"][0];
-    if (startEvent) {
-      const startMsg = startEvent.html_message;
-      var re = /([0-9]+(_[0-9]+)+).*[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]{1,3})?/;
-      var startTime = re.exec(startMsg)[0];
-      spawnEvents[id][startTime] = spawnEvents[id]["latest"];
-      spawnEvents[id]["latest"] = [];
-      $(`#${id}-log-select`)
-        .append(`<option value="${startTime}">${startTime}</option>`)
-        .val("latest");
-    }
   }
 });
---
+++ static/js/home/utils.js
@@ -0,0 +1,135 @@
+define(["jquery"], function ($) {
+  "use strict";
+
+  const progressStates = {
+    "running": {
+      "text": "running",
+      "background": "bg-success",
+      "width": 100
+    },
+    "stop_failed": {
+      "text": "running (stop failed)",
+      "background": "bg-success",
+      "width": 100
+    },
+    "cancelling": {
+      "text": "cancelling...",
+      "background": "bg-danger",
+      "width": 100
+    },
+    "stopping": {
+      "text": "stopping...",
+      "background": "bg-danger",
+      "width": 100
+    },
+    "failed": {
+      "text": "last spawn failed",
+      "background": "bg-danger",
+      "width": 100
+    },
+    "reset": {
+      "text": "",
+      "background": "",
+      "width": 0
+    }
+  }
+
+  var parseJSON = function (inputString) {
+    try {
+      return JSON.stringify(JSON.parse(inputString), null, 2);
+    } catch (e) {
+      return inputString;
+    }
+  }
+
+  var getId = function (element, slice_index = -2) {
+    let id_array = $(element).attr("id").split('-');
+    let id = id_array.slice(0, slice_index).join('-');
+    return id;
+  }
+
+  var getLabConfigSelectValues = function (id) {
+    return {
+      "service": $(`select#${id}-version-select`).val(),
+      "system": $(`select#${id}-system-select`).val(),
+      "flavor": $(`select#${id}-flavor-select`).val(),
+      "account": $(`select#${id}-account-select`).val(),
+      "project": $(`select#${id}-project-select`).val(),
+      "partition": $(`select#${id}-partition-select`).val(),
+    }
+  }
+
+  var setLabAsNA = function (id, reason) {
+    $(`#${id}-start-btn, #${id}-open-btn, #${id}-cancel-btn, #${id}-stop-btn`).addClass("d-none disabled");
+    $(`#${id}-na-btn`).removeClass("d-none");
+    $(`#${id}-na-status`).html(1);
+    $(`#${id}-na-info`).removeClass("d-none").html(reason);
+  }
+
+  var updateProgressState = function (id, state) {
+    $(`#${id}-progress-bar`)
+      .width(progressStates[state].width)
+      .removeClass("bg-success bg-danger")
+      .addClass(progressStates[state].background)
+      .html("");
+    $(`#${id}-progress-info-text`).html(progressStates[state].text);
+  }
+
+  var appendToLog = function (log, htmlMsg) {
+    try { htmlMsg = htmlMsg.replace(/&nbsp;/g, ' '); }
+    catch (e) { return; } // Not a valid htmlMsg
+    // Only append if a log message has not been appended yet
+    var exists = false;
+    log.children().each(function (i, e) {
+      let logMsg = $(e).html();
+      if (htmlMsg == logMsg) exists = true;
+    })
+    if (!exists)
+      log.append($('<div class="log-div">').html(htmlMsg));
+  }
+
+  var updateSpawnEvents = function (spawnEvents, id) {
+    if (spawnEvents[id]["latest"].length) {
+      var re = /([0-9]+(-[0-9]+)+).*[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]{1,3})?/;
+      for (const [_, event] of spawnEvents[id]["latest"].entries()) {
+        const startMsg = event.html_message || event.message;
+        const startTimeMatch = re.exec(startMsg);
+        if (startTimeMatch) {
+          const startTime = startTimeMatch[0];
+          // Have we already created a log entry for this time?
+          let logOptions = $(`#${id}-log-select option`);
+          let logTimes = $.map(logOptions, function (option) {
+            return option.value;
+          });
+          // If yes, we do not need to do anything anymore
+          if (logTimes.includes(startTime)) return;
+          // Otherwise, save the current events to the startTime,
+          // update the log select and reset the "latest" events.
+          spawnEvents[id][startTime] = spawnEvents[id]["latest"];
+          spawnEvents[id]["latest"] = [];
+          $(`#${id}-log-select`)
+            .append(`<option value="${startTime}">${startTime}</option>`)
+            .val("latest");
+          break;
+        }
+      }
+      // We didn't manage to find a time, so update with no timestamp
+      if ((spawnEvents[id]["latest"].length)) {
+        spawnEvents[id]["previous"] = spawnEvents[id]["latest"];
+        spawnEvents[id]["latest"] = [];
+      }
+    }
+  }
+
+  var utils = {
+    parseJSON: parseJSON,
+    getId: getId,
+    getLabConfigSelectValues: getLabConfigSelectValues,
+    setLabAsNA: setLabAsNA,
+    updateProgressState: updateProgressState,
+    appendToLog: appendToLog,
+    updateSpawnEvents: updateSpawnEvents,
+  };
+
+  return utils;
+})
